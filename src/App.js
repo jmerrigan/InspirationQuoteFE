40f514980f1022a3cfb7b05007edb4b3c37d3b1b
James Merrigan <merrigan.james@gmail.com>
2019-01-18 14:25:15 +1100

    Styled quote display box and linked with Unsplash to grab a random BG image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+
 class App extends Component {
+  state = { quote: 'Loading....',
+            author: 'Loading...',
+            image: ''
+        }
+
+  componentDidMount() {
+
+    const url = 'http://localhost:5000/api'
+    axios.get(url)
+      .then(res => {
+        const quote = res.data.content;
+        const author = res.data.attribution;
+        this.setState({ quote, author })
+      })
+
+
+      
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>A Daily Dose of Inspiration</h1>
+        <div className="center-div">
+          <p id="quoteDisplay">{this.state.quote}</p>
+          <p id="authorDisplay">~ {this.state.author}</p>
+        </div>  
       </div>
     );
   }
